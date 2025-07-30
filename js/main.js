e1fd49b3a83d34291c8b2242ffb97e296aca5e56
SJ23y <mcfolland@gmail.com>
2025-07-30 12:44:44 +0300

    feat: added scripts for open/close modal, cart-popup and sub-catalog

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector('.modal');
+const modalCloseButton = modal.querySelector('.modal-close-button');
+const openModalButtons = document.querySelectorAll('.delivery-button')
+const cartButton = document.querySelector('.cart-button');
+const cartPopup = document.querySelector('.cart-popup');
+const dropdownButton = document.querySelector('.dropdown-button');
+
+
+if (modal) {
+  openModalButtons.forEach(button => {
+    if (button) {
+       button.addEventListener('click', () =>  modal.classList.toggle('active'));
+    }
+
+  });
+  if (modalCloseButton) {
+    modalCloseButton.addEventListener('click', () => modal.classList.toggle('active'));
+  }
+}
+
+if (cartPopup && cartButton) {
+  cartButton.addEventListener('click', () => cartPopup.classList.toggle('active'));
+}
+
+if (dropdownButton) {
+  dropdownButton.addEventListener('click', () => dropdownButton.classList.toggle('active'))
+}
